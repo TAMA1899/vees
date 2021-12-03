aec11a331031bfb773ea401a328368ff10cdcdbc
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-12-03 17:34:18 +0700

    Update ytsearch.py

--- plugins/ytsearch.py
+++ plugins/ytsearch.py
@@ -7,18 +7,19 @@ from youtube_search import YoutubeSearch
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search (masukan judul lagu)!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("🔎 Mencari Lagu...")
         results = YoutubeSearch(query, max_results=5).to_dict()
         text = ""
         for i in range(5):
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"Judul : {results[i]['title']}\n"
+            text += f"Durasi : {results[i]['duration']}\n"
+            text += f"Views : {results[i]['views']}\n"
+            text += f"Channel : {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"─────────────────────────────\n"
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
         await message.reply_text(str(e))
