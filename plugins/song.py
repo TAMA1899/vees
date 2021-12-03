917d4ab9304d89274cd4ed85c6b82ba342175718
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-12-03 17:16:54 +0700

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -23,7 +23,7 @@ def song(client, message):
 
     query = "".join(" " + str(i) for i in message.command[1:])
     print(query)
-    m = message.reply("🔎 Finding the song...")
+    m = message.reply("🔄 **Mencari Lagu**...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -41,17 +41,17 @@ def song(client, message):
 
     except Exception as e:
         m.edit(
-            "✖️ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
+            "❁ <b>Lagu</b> tidak ditemukan.\n❁ Ketik /song (judul lagu)."
         )
         print(str(e))
         return
-    m.edit("`Downloading Song... Please wait ⏱`")
+    m.edit("📥 Sedang Mendownload Lagu")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @{BOT_USERNAME} "
+        rep = f"📋 **Judul** : [{title[:35]}]({link})\n⏱️ **Durasi** : `{duration}`\n📤 **By**: @{BOT_USERNAME} "
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
