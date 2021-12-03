f20428c8de7753435a09ee96267ebd8d8889d3aa
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-12-03 22:34:26 +0700

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -16,7 +16,6 @@ def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
 
-
 @Client.on_message(filters.command(["song"]))
 def song(clien, message):
   
